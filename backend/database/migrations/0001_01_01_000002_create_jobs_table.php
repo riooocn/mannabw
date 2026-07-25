bfd1e77a441ca44e8323fcb32950c19364101304
riooocn <davidriochristian43@gmail.com>
2026-07-25 14:23:35 +0700

    Fix jobs composite index string lengths

--- backend/database/migrations/0001_01_01_000002_create_jobs_table.php
+++ backend/database/migrations/0001_01_01_000002_create_jobs_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('jobs', function (Blueprint $table) {
             $table->id();
-            $table->string('queue')->index();
+            $table->string('queue', 191)->index();
             $table->longText('payload');
             $table->unsignedSmallInteger('attempts');
             $table->unsignedInteger('reserved_at')->nullable();
@@ -37,8 +37,8 @@ return new class extends Migration
         Schema::create('failed_jobs', function (Blueprint $table) {
             $table->id();
             $table->string('uuid', 191)->unique();
-            $table->string('connection');
-            $table->string('queue');
+            $table->string('connection', 191);
+            $table->string('queue', 191);
             $table->longText('payload');
             $table->longText('exception');
             $table->timestamp('failed_at')->useCurrent();
