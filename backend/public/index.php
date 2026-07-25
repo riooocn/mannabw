771402ff704bf9447e53e36b586b9d64facb16dd
riooocn <davidriochristian43@gmail.com>
2026-07-25 14:57:58 +0700

    Override TMPDIR to fix tmpfile() permission denied on InfinityFree

--- backend/public/index.php
+++ backend/public/index.php
@@ -13,6 +13,9 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 // Register the Composer autoloader...
 require __DIR__.'/../vendor/autoload.php';
 
+// Override system temporary directory for InfinityFree shared hosting
+putenv('TMPDIR=' . __DIR__ . '/../storage/app/livewire-tmp');
+
 // Bootstrap Laravel and handle the request...
 /** @var Application $app */
 $app = require_once __DIR__.'/../bootstrap/app.php';
